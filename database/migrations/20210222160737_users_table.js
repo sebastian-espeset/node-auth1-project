06b54e46ef408c1cd97474b02647288c4ae2e49f
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-22 16:29:22 -0600

    seeded data functioning

--- database/migrations/20210222160737_users_table.js
+++ database/migrations/20210222160737_users_table.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema
-    .createtable("users",table=>{
+    .createTable("users",table=>{
         table.increments("user_id")
         table.string("user_name",128).notNullable().unique();
         table.string("password",128).notNullable();
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./dev.sqlite3",
+      filename: "./database/users.db3",
     },
     pool: {
       afterCreate: (conn, done) => {
