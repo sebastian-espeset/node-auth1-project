fe146295d3639265e501f63f105882bd9838d64b
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-24 10:52:34 -0600

    empty commit

--- database/seeds/01-users-table.js
+++ database/seeds/01-users-table.js
@@ -5,9 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {user_name: "jake", password: "password"},
-        {user_name: "jess", password:"password"},
-        {user_name: "al", password: "password"}
+        {user_name: "sebastian", password: "password"}
       ]);
     });
 };
